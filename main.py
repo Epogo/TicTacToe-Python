17782f99c0e6884c2c8404adc0ea2eb5fe9bd9bc
Epogo <62990985+Epogo@users.noreply.github.com>
2020-05-01 22:11:13 +0300

    Update main.py
    Updated N.1

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-'''
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
 #Autor:Evgeni Pogoster
 #Tic Tac Toe Game 
-#Input players names
 
-
-
-
+#Initilization of paramaeters
 isWinner=False;
 turn=0;
 playingBoard=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
 
 nameA=input("player A, Enter your name: ");
 nameB=input("player B, Enter your name: ");
+
+#Prints the Board
 def print_board():
     print(' '+' 1 '+'  2 '+'  3');
     print('1 '+playingBoard[0][0]+' | '+playingBoard[0][1]+' | '+playingBoard[0][2]);
@@ -27,7 +18,19 @@ def print_board():
     print('  '+'---------');
     print('3 '+playingBoard[2][0]+' | '+playingBoard[2][1]+' | '+playingBoard[2][2]);
 
-def CheckWin:
+#Checking if there is a winner
+def CheckWin():
+    for i in range(2):
+        if ((playingBoard[i][0]==playingBoard[i][1]) and (playingBoard[i][1]==playingBoard[i][2]) and playingBoard[i][0]!=' '):
+            return True;
+        elif ((playingBoard[0][i]==playingBoard[1][i]) and (playingBoard[1][i]==playingBoard[2][i]) and playingBoard[0][i]!=' '):
+            return True;
+    if ((playingBoard[0][0]==playingBoard[1][1]) and (playingBoard[1][1]==playingBoard[2][2]) and playingBoard[0][0]!=' '):
+        return True;
+    elif ((playingBoard[0][2]==playingBoard[1][1]) and (playingBoard[1][1]==playingBoard[2][0]) and playingBoard[0][2]!=' '):
+        return True;
+    else:
+        return False;
 
 #Game Process
 while not(isWinner):
@@ -35,9 +38,12 @@ while not(isWinner):
     if(turn%2==0):
         print(nameA+' it is your turn');
         print('Where to put X?');
-        i = int(input("Enter a row number: "))-1;
-        j = int(input("Enter a column number: "))-1;
-        if (0<=j and j<3 and 0<=i and i<3 and isinstance(i, int) and isinstance(j, int)):
+        i = int(input("Enter a row number: "));
+        j = int(input("Enter a column number: "));
+        if (isinstance(i, int) and isinstance(j, int)):
+            i=i-1;
+            j=j-1;
+        if (0<=j and j<3 and 0<=i and i<3):
             if(playingBoard[i][j]!=' '):
                 print("Sorry, Cell is occupied");
                 continue;
@@ -61,11 +67,21 @@ while not(isWinner):
         else:
             print("Illegal Input");
             continue;
+    if(turn<4):
+        turn=turn+1;
+        continue;
+    
+    if (CheckWin()):
+        print_board();
+        if(turn%2==0):
+            print (nameA+' is the Winner!');
+        else:
+            print (nameB+' is the Winner!');
+        isWinner==True;
+        break;
+    if(turn==8):
+        print_board();
+        print("It's a Draw!");
+        break;
         
     turn=turn+1;
-        
-    
-
-
-
-
